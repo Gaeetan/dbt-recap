836cbf111c552f7f0454abe5c3a01ce0c6c3b110
Gaëtan Manchon <gaetan.manchon@lewagon.org>
2023-05-10 07:50:37 +0000

    remove examples + correct lineage

--- models/sales/late_orders_per_city.sql
+++ models/sales/late_orders_per_city.sql
@@ -5,6 +5,6 @@ SELECT
 
 FROM {{ ref('late_delivered_orders') }} AS orders
 
-JOIN data_analytics_olist.customers AS customers USING(customer_id)
+JOIN {{ source('data_analytics_olist','customers')}} AS customers USING(customer_id)
 
 GROUP BY customers.customer_city
